72cc0e280f79802d5c4a74d40e3b3e78991cf298
Kevin Amiranoff <kamiranoff@gmail.com>
2018-07-19 00:20:25 +0100

    little wolvie animation

--- SpriteKit Demo/GameScene.swift
+++ SpriteKit Demo/GameScene.swift
@@ -10,11 +10,57 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+//    let myFirstNode = SKNode();
+//    let myFirstSpriteNode = SKSpriteNode(color: UIColor.red, size: CGSize(width: 200.0, height: 200.0));
+//    let myFirstTextureNode = SKSpriteNode(imageNamed: "wolverine");
+//    let blueBox = SKSpriteNode(color: UIColor.blue, size: CGSize(width: 100.0, height: 100.0));
+    
+    let wolverineSprite = SKSpriteNode(imageNamed: "sprites0");
+    var wolverineFrames = [SKTexture]();
     
     override func didMove(to view: SKView) {
         
-    
+        wolverineSprite.position = CGPoint(x: frame.midX, y: frame.midY);
+        addChild(wolverineSprite);
+        
+        let textureAtlas = SKTextureAtlas(named: "Player Frames");
+        for index in 0..<textureAtlas.textureNames.count {
+            let textureName = "Sprites" + String(index);
+            wolverineFrames.append(textureAtlas.textureNamed(textureName));
+        }
+//        addChild(myFirstNode);
+//
+//        myFirstSpriteNode.position = CGPoint(x: frame.midX, y: frame.midY)
+//        myFirstSpriteNode.anchorPoint = CGPoint.zero
+//        addChild(myFirstSpriteNode);
+//
+//        myFirstTextureNode.zPosition = 1;
+//        myFirstTextureNode.size = CGSize(width: 100.0, height: 100.0);
+//        myFirstSpriteNode.addChild(myFirstTextureNode);
+//
+//        blueBox.zPosition = 2;
+//        blueBox.position = CGPoint(x: myFirstSpriteNode.size.width/2, y: myFirstSpriteNode.size.height/2)
+//        myFirstSpriteNode.addChild(blueBox);
+//
+//        physicsBody = SKPhysicsBody(edgeLoopFrom: frame);
+//        myFirstTextureNode.physicsBody = SKPhysicsBody(circleOfRadius: myFirstTextureNode.size.width/2);
+//        myFirstTextureNode.physicsBody?.allowsRotation = true;
+//        myFirstTextureNode.physicsBody?.restitution = 0.7;
     }
     
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let animate = SKAction.animate(with: wolverineFrames, timePerFrame: 0.1);
+        let forever = SKAction.repeatForever(animate);
+        
+        if let _ = wolverineSprite.action(forKey: "run") {
+            wolverineSprite.removeAllActions();
+        } else {
+            wolverineSprite.run(forever, withKey: "run");
+
+        }
+        
+    }
 
 }
